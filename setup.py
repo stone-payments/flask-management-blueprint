fe612b0a766e1d97ed8568cd0ebaf674a79158b7
Vinicius Amaral <vcamaral@gmail.com>
2018-06-06 17:08:53 -0300

    Adiciona dependências no setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ from setuptools import setup, find_packages
 
 setup(
     description='Flask Management Blueprint',
-    install_requires=['aiofiles', 'flask', 'websockets'],
+    install_requires=['aiofiles', 'flask', 'websockets', 'async_timeout', 'aiohttp'],
     long_description=open('README.md').read().strip(),
     name='flask_management_blueprint',
     packages=find_packages(exclude=['tests']),
     py_modules=['flask_management_blueprint'],
     url='https://github.com/stone-payments/flask-management-blueprint',
-    version='0.1.0',
+    version='0.1.1',
 )
